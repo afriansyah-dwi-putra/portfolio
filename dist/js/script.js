5df938da9108163252f9aa286dc81d3635e2dbb9
afriansyah dwi putra <afriansyan.dwi.putra@gmail.com>
2025-06-02 09:44:13 +0700

    screens fixed

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,19 @@
 // Navbar fixed
-// window.onscroll = function() {
-//     const header = document.querySelector('header');
-//     const fixedNav = header.offsetTop;
-//     // const toTop= document.querySelector( '#to-top' );
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    // const toTop= document.querySelector( '#to-top' );
 
-//     if(window.pageYOffset > fixedNav) {
-//     header.classList.add('navbar-fixed');
-//     // toTop.classList.remove('hidden');
-//     // toTop.classList.add('flex');
-// } else {
-//     header.classList.remove('navbar-fixed');
-//     // toTop.classList.remove('flex');
-//     // toTop.classList.add('hidden');
-//     }
-// };
+    if(window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+    // toTop.classList.remove('hidden');
+    // toTop.classList.add('flex');
+} else {
+    header.classList.remove('navbar-fixed');
+    // toTop.classList.remove('flex');
+    // toTop.classList.add('hidden');
+    }
+};
 
 // Hamburger
     const hamburger = document.querySelector('#hamburger');
